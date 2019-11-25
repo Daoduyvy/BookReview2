9e382c5c4e5a3598792f2395ec37568850dd16a7
Daoduyvy <mrduyvivbhp@gmail.com>
2019-11-25 15:24:07 +0700

    23-11-2019

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,44 +1,50 @@
 class BooksController < ApplicationController
 
-	before_action :find_book, only: [:show,:edit,:update,:destroy]
-	BOOK_SIZE = 2
-	def index
-		@page = (params[:page] || 0 ).to_i
-		@books = Book.offset(BOOK_SIZE*@page).limit(BOOK_SIZE)
-	end
+  before_action :find_book, only: [:show,:edit,:update,:destroy]
+  BOOK_SIZE = 2
+  def index
+    @page = (params[:page] || 0 ).to_i
+    @books = if params[:term]
+      Book.where('title LIKE ?', "%#{params[:term]}%")
+     else
+    
+    Book.offset(BOOK_SIZE*@page).limit(BOOK_SIZE)
+    end
 
-	def new
-		@book = current_user.books.build
-		@categories = Category.all.map{| c | [c.name, c.id] }
-	end
+  end
 
-	def show
-		
-	end
+  def new
+    @book = current_user.books.build
+    @categories = Category.all.map{| c | [c.name, c.id] }
+  end
 
-	def update
-		if @book.update(book_params)
-			redirect_to book_path(@book)
-		else
-			render 'edit'
-		end	
-	end
+  def show
+    
+  end
 
-	def create
-		@book = current_user.books.build(book_params)
-		@book.category_id = params[:category_id]
-		if @book.save
-			redirect_to root_path
-		else
-			render 'new'	
-		end
-	end
+  def update
+    if @book.update(book_params)
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end 
+  end
 
-	private 
-		def book_params
-			params.require(:book).permit(:title,:publish_date,:author,:category_id)
-		end
-		def find_book
-			@book = Book.find(params[:id])
-		end
+  def create
+    @book = current_user.books.build(book_params)
+    @book.category_id = params[:category_id]
+    if @book.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private 
+    def book_params
+      params.require(:book).permit(:title,:publish_date,:author,:category_id)
+    end
+    def find_book
+      @book = Book.find(params[:id])
+    end
 end
