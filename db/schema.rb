076a930a56bc3ef3735a8db86214a1f82441fb49
Daoduyvy <mrduyvivbhp@gmail.com>
2019-11-25 15:24:07 +0700

    23-11-2019

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_25_033127) do
+ActiveRecord::Schema.define(version: 2019_11_26_021830) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,10 @@ ActiveRecord::Schema.define(version: 2019_11_25_033127) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.integer "category_id"
+    t.string "book_img_file_name"
+    t.string "book_img_content_type"
+    t.integer "book_img_file_size"
+    t.datetime "book_img_updated_at"
   end
 
   create_table "categories", force: :cascade do |t|
